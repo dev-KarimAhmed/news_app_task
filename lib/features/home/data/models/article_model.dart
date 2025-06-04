cdddb7e9acc2f64690da04fdd2efcb1b5bd634b0
Karim Ahmed <devKarimAhmed@gmail.com>
2025-06-04 19:37:10 +0300

    create simple ui with fake data

---
+++ lib/features/home/data/models/article_model.dart
@@ -0,0 +1,13 @@
+// Mock data class for articles
+class Article {
+  final String title;
+  final String urlToImage;
+  final String publishedAt;
+
+  const Article({
+    required this.title,
+    required this.urlToImage,
+    required this.publishedAt,
+  });
+}
+
