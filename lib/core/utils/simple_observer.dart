42dbd40ba249b79cc9cd0074fd83d06c5c5a4729
Karim Ahmed <devKarimAhmed@gmail.com>
2025-06-05 05:50:17 +0300

    Add SimpleBlocObserver for enhanced Bloc state logging in main.dart

---
+++ lib/core/utils/simple_observer.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+
+@override
+  void onChange(BlocBase bloc, Change change) {
+    log(change.toString());
+    super.onChange(bloc, change);
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    log('onCreate: ${bloc.runtimeType}');
+    super.onCreate(bloc);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    log('onClose: ${bloc.runtimeType}');
+    super.onClose(bloc);
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/core/utils/di_injection.dart';
+import 'package:news_app/core/utils/simple_observer.dart';
 import 'package:news_app/features/home/presentation/views/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  Bloc.observer = SimpleBlocObserver();
   setupServiceLocator();
   runApp(const NewsApp());
 }
